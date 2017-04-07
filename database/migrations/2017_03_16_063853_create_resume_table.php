76e9ad8a07a03087522b355e41507ddd5a3ce4d9
小强 <7903783735@qq.com>
2017-04-07 15:02:48 +0800

    update migration

--- database/migrations/2017_03_16_063853_create_resume_table.php
+++ database/migrations/2017_03_16_063853_create_resume_table.php
@@ -20,9 +20,9 @@ class CreateResumeTable extends Migration
             $table->timestamp('birth')->comment('生日');
             $table->string('avatar',100)->comment('头像');
             $table->string('mobile',11)->comment('手机号');
-            $table->string('email',50)->comment('邮箱');
+            $table->string('email',50)->nullable()->comment('邮箱');
             $table->string('education',20)->comment('学历');
-            $table->string('specialty',50)->comment('专业');
+            $table->string('specialty',50)->nullable()->comment('专业');
             $table->string('hobby',255)->comment('爱好');
             $table->text('skill')->comment('技能');
             $table->timestamps();
